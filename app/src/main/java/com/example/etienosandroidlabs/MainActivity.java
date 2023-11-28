eb57f7b52b204104f5e767098172c2d1b07dd519
Etieno Josiah <etijosiah27@gmail.com>
2023-11-27 21:35:25 -0500

    Final merged master branch

--- app/src/main/java/com/example/etienosandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/etienosandroidlabs/MainActivity.java
@@ -15,12 +15,6 @@ public class MainActivity extends AppCompatActivity {
     /** This holds the login button*/
     private Button btn;
 
-    /** This holds the text shown at the center of the screen*/
-    private TextView tv;
-    /** This holds the password input from user*/
-    private EditText et;
-    /** This holds the login button*/
-    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
