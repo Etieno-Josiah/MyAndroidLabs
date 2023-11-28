be6b0634114448ea6354e837e651614d76010895
Etieno Josiah <etijosiah27@gmail.com>
2023-11-27 21:18:15 -0500

    Final version of master branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,9 +3,11 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+val room_version = "2.6.0"
+
 android {
     namespace = "com.example.etienosandroidlabs"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.etienosandroidlabs"
@@ -15,6 +17,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    viewBinding{
+        enable = true;
     }
 
     buildTypes {
@@ -33,15 +39,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+    testImplementation("junit:junit:")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("androidx.room:room-runtime:$room_version")
+    annotationProcessor ("androidx.room:room-compiler:$room_version")
 }
--- app/src/main/java/com/example/etienosandroidlabs/MainActivity.java
+++ app/src/main/java/com/example/etienosandroidlabs/MainActivity.java
@@ -2,105 +2,12 @@ package com.example.etienosandroidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    /** This holds the text at the center of the screen*/
-    private TextView tv;
-    /** This holds the password input from user*/
-    private EditText et;
-    /** This holds the login button*/
-    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tv = findViewById(R.id.textView);
-        et = findViewById(R.id.editPassword);
-        btn = findViewById(R.id.loginButton);
-
-        btn.setOnClickListener(clk -> {
-            String password = et.getText().toString();
-            checkPasswordComplexity(password);
-
-            if(checkPasswordComplexity(password)){
-                tv.setText(R.string.password_good);
-            }else{
-                tv.setText(R.string.password_bad);
-            }
-        });
-    }
-
-    /**This function checks a password input is complex enough.
-     *
-     * @param password The string that we are checking
-     * @return Returns true if string is complex enough and false if it is not complex enough.
-     */
-    boolean checkPasswordComplexity(String password){
-        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
-        foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
-
-        for (int i = 0; i <password.length(); i++){
-            char currentChar = password.charAt(i);
-            if (Character.isUpperCase(currentChar)){
-                foundUpperCase = true;
-            }
-            if (Character.isLowerCase(currentChar)){
-                foundLowerCase = true;
-            }
-            if (Character.isDigit(currentChar)){
-                foundNumber = true;
-            }
-            if (isSpecialCharacter(currentChar)){
-                foundSpecial = true;
-            }
-        }
-
-        if (!foundUpperCase){
-            Toast.makeText(getApplicationContext(), "Add at least one upper case letter",
-                    Toast.LENGTH_SHORT).show();
-            return false;
-        }else if (!foundLowerCase){
-            Toast.makeText(getApplicationContext(), "Add at least one lower case letter",
-                    Toast.LENGTH_SHORT).show();
-            return false;
-        }else if (!foundNumber) {
-            Toast.makeText(getApplicationContext(), "Add at least one number",
-                    Toast.LENGTH_SHORT).show();
-            return false;
-        }else if (!foundSpecial){
-            Toast.makeText(getApplicationContext(), "Add at least one of the special" +
-                    "characters", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
-
-    /** This checks if a password contains any special characters
-     *
-     * @param c each character in the password string
-     * @return Returns true if there is at least special character and false if there is no special character
-     */
-    public boolean isSpecialCharacter(char c){
-        switch (c){
-            case '!':
-            case '@':
-            case '#':
-            case '$':
-            case '%':
-            case '^':
-            case '*':
-            case '?':
-                return true;
-            default:
-                return false;
-        }
     }
 }
